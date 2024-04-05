59e921787aec69231b32b4f14cefaba3ed8c64c1
Amul <amulgairola100@gmail.com>
2024-04-05 12:23:04 +0530

    demo commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import News from './components/News'
+import './App.css'
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='neww'> 
          <NavBar/>
          <News/>
         </div>  
--- src/components/News.js
+++ src/components/News.js
@@ -1,72 +1,62 @@
-import React, { Component } from 'react';
-// import NewItems from './NewItems';
+import React, { useState, useEffect } from 'react';
 import NewItems from './NewItems';
 
-export class News extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-    }
-  }
+const News = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c69feb27cd2947058e6e72285ca84cac&page=1&pageSize=20";
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
-  }
-
-  clickPev = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c69feb27cd2947058e6e72285ca84cac&page=${this.state.page - 1}&pageSize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles
-    }
-    )
-  }
-
-  clickNext = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
-
-    }
-    else {
-     console.log("next has been clicked")
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c69feb27cd2947058e6e72285ca84cac&page=${this.state.page + 1}&pageSize=20`;
+  useEffect(() => {
+    const fetchArticles = async () => {
+      setLoading(true);
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c69feb27cd2947058e6e72285ca84cac&page=${page}&pageSize=20`;
       let data = await fetch(url);
       let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles
-      }
-      )
+      setArticles(parsedData.articles);
+      setTotalResults(parsedData.totalResults);
+      setLoading(false);
+    };
+    fetchArticles();
+  }, [page]);
+
+  const clickPrev = async () => {
+    if (page > 1) {
+      setPage(page - 1);
     }
+  };
 
-  }
+  const clickNext = async () => {
+    if (page + 1 <= Math.ceil(totalResults / 20)) {
+      setPage(page + 1);
+    }
+  };
 
-
-  render() {
-    return (
-      <div className="container my-3">
-        <h2>NewsApp</h2>
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return <div className="col-md-3" key={element.url} >
-              <NewItems title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} url={element.url} />
-            </div>
-          })}
-        </div>
-        <div className="continer my-3 d-flex justify-content-between">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.clickPev}>&laquo; Previous</button>
-          <button disbaled={this.state.pg} className="btn btn-dark" onClick={this.clickNext}>Next &raquo;</button>
-        </div>
+  return (
+    <div className="container my-3">
+      <h2>NewsApp</h2>
+      <div className="row">
+        {articles.map((element) => (
+          <div className="col-md-3" key={element.url}>
+            <NewItems
+              title={element.title ? element.title.slice(0, 45) : ''}
+              description={element.description ? element.description.slice(0, 88) : ''}
+              imageUrl={element.urlToImage}
+              url={element.url}
+            />
+          </div>
+        ))}
       </div>
-    )
-  }
-}
+      <div className="container my-3 d-flex justify-content-between">
+        <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={clickPrev}>
+          &laquo; Previous
+        </button>
+        <button disabled={page >= Math.ceil(totalResults / 20)} className="btn btn-dark" onClick={clickNext}>
+          Next &raquo;
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default News
+export default News;
